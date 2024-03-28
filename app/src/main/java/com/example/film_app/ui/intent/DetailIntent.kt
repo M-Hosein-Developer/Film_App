8aff60810657e80c221bc18bdc2cc9e9dc81debe
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-03-28 09:12:04 -1200

    Create Detail View Model

--- app/src/main/java/com/example/film_app/ui/intent/DetailIntent.kt
+++ app/src/main/java/com/example/film_app/ui/intent/DetailIntent.kt
@@ -2,6 +2,6 @@ package com.example.film_app.ui.intent
 
 sealed class DetailIntent {
 
-    data object fetchTrend : DetailIntent()
+    data object fetchAllData : DetailIntent()
 
 }
---
+++ app/src/main/java/com/example/film_app/viewModel/DetailViewModel.kt
@@ -0,0 +1,77 @@
+package com.example.film_app.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.film_app.model.repository.detailRepo.DetailRepository
+import com.example.film_app.ui.intent.DetailIntent
+import com.example.film_app.ui.state.detailState.DetailState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(private val repository: DetailRepository) : ViewModel() {
+
+    val dataIntent = Channel<DetailIntent>()
+
+    //All Data
+    private val _allDataState = MutableStateFlow<DetailState>(DetailState.Idle)
+    val allDataState : StateFlow<DetailState> get() = _allDataState
+
+    init {
+        handleIntent()
+    }
+
+    private fun handleIntent() {
+
+        viewModelScope.launch {
+
+            dataIntent.consumeAsFlow().collect{
+
+                when(it){
+
+                    DetailIntent.fetchAllData -> getAllDataFromDb()
+
+                }
+
+            }
+
+        }
+
+    }
+
+    private fun getAllDataFromDb() {
+
+        _allDataState.value = DetailState.Loading
+        viewModelScope.launch {
+
+            repository.allDate.catch {
+                _allDataState.value = DetailState.AllDataError(it.localizedMessage)
+            }.collect{
+                _allDataState.value = DetailState.AllData(it)
+            }
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
