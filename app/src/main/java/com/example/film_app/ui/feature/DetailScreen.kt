fd7f7f288f1a5bb1f86f7dfe75325291b3896155
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-03-28 22:56:53 -1200

    Create TopAppBar Detail Screen

--- app/src/main/java/com/example/film_app/ui/feature/DetailScreen.kt
+++ app/src/main/java/com/example/film_app/ui/feature/DetailScreen.kt
@@ -3,7 +3,15 @@ package com.example.film_app.ui.feature
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -12,6 +20,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.film_app.ui.intent.DetailIntent
 import com.example.film_app.ui.state.detailState.DetailState
@@ -61,9 +73,40 @@ fun DetailScreen(viewModel: DetailViewModel, navController: NavHostController, m
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Text(text = detailData.toString())
+        DetailToolbar(
+            { navController.popBackStack() },
+            { }
+        )
 
     }
 
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailToolbar(onBackCLicked: () -> Unit, onFavoriteClicked: () -> Unit) {
+
+    TopAppBar(
+        title = { Text(
+            text = "Detail",
+            Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+        ) },
+        navigationIcon = {
+            IconButton(onClick = { onBackCLicked.invoke() }) {
+                Icon(imageVector = Icons.Default.KeyboardArrowLeft, contentDescription = null)
+            }
+        },
+        actions = {
+            IconButton(onClick = { onFavoriteClicked.invoke() }) {
+                Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = null)
+            }
+        },
+        modifier = Modifier.fillMaxWidth()
+        )
 
 }
