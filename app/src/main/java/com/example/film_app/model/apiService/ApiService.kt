c4001ad06a3f2c71d2de84d9de805cfab5073c6c
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-04-06 06:46:36 -1200

    Provide Api

--- app/src/main/java/com/example/film_app/model/apiService/ApiService.kt
+++ app/src/main/java/com/example/film_app/model/apiService/ApiService.kt
@@ -2,8 +2,10 @@ package com.example.film_app.model.apiService
 
 import com.example.film_app.model.dataClass.ResponseMovies
 import com.example.film_app.util.API_KEY
+import com.example.movies.model.apiService.TrailerResponse
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 
 interface ApiService {
 
@@ -26,5 +28,10 @@ interface ApiService {
     @Headers(API_KEY)
     @GET("3/trending/all/day?language=en-US")
     suspend fun getAllTrend() : ResponseMovies
+
+    @Headers(API_KEY)
+    @GET("3/movie/{id}/videos?language=en-US")
+    suspend fun getTrailerById(@Path("id") id : Int) : TrailerResponse
+
 
 }
