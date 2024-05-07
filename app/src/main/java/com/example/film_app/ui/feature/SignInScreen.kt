56b54e8cf188db3ef2da67fcfa46c391ccda5a0b
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-05-07 07:40:01 -1200

    Setup SignIn Screen Logic Of Firebase

--- app/src/main/java/com/example/film_app/ui/feature/SignInScreen.kt
+++ app/src/main/java/com/example/film_app/ui/feature/SignInScreen.kt
@@ -1,5 +1,6 @@
 package com.example.film_app.ui.feature
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,14 +20,17 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -34,14 +38,33 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.film_app.ui.intent.RegisterIntent
+import com.example.film_app.ui.state.registerState.SignInState
 import com.example.film_app.util.BottomNavItem
 import com.example.film_app.viewModel.RegisterViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun SignInScreen(navController: NavHostController, registerViewModel: RegisterViewModel) {
 
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+
+    LaunchedEffect(Unit) {
+        registerViewModel.signInState.collect {
+            when (it) {
+                is SignInState.Idle -> {}
+                is SignInState.IsRegister -> {
+                    Toast.makeText(context, it.success, Toast.LENGTH_SHORT).show()
+                }
+                is SignInState.RegisterError -> {
+                    Toast.makeText(context, it.error, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 
     Column(
         Modifier.fillMaxSize(),
@@ -63,10 +86,24 @@ fun SignInScreen(navController: NavHostController, registerViewModel: RegisterVi
 
         Button(
             onClick = {
-                navController.navigate(BottomNavItem.HomeScreen.rout) {
-                    popUpTo(BottomNavItem.SignInScreen.rout) {
-                        inclusive = true
+                if (password.isNotEmpty() && username.isNotEmpty()) {
+                    scope.launch {
+                        registerViewModel.dataIntent.send(RegisterIntent.SignIn(username, password))
                     }
+                    navController.navigate(BottomNavItem.HomeScreen.rout) {
+                        popUpTo(BottomNavItem.SignInScreen.rout) {
+                            inclusive = true
+                        }
+                    }
+
+                    Toast.makeText(context, "SignIn Successful", Toast.LENGTH_SHORT).show()
+
+                }else{
+                    Toast.makeText(
+                        context,
+                        "Sign In Failed",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             },
             shape = RectangleShape,
