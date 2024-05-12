75e3cc036a99962d99393017ec8db907539708f3
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-05-11 19:02:51 -1200

    Setup Translate Ui

--- app/src/main/java/com/example/film_app/ui/feature/SettingScreen.kt
+++ app/src/main/java/com/example/film_app/ui/feature/SettingScreen.kt
@@ -1,17 +1,25 @@
 package com.example.film_app.ui.feature
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -31,7 +39,7 @@ fun SettingScreen(navController: NavHostController) {
             navController.popBackStack()
         }
 
-
+        Translator(){}
 
     }
 
@@ -62,3 +70,43 @@ fun SettingToolbar(onBackPressed :() -> Unit){
 
 }
 
+@Composable
+fun Translator(onTranslateClicked : (Boolean) -> Unit){
+
+    Row(
+        Modifier.fillMaxWidth()
+            .padding(horizontal = 18.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        Text(
+            text = "Translate",
+            style = TextStyle(
+                fontSize = 18.sp
+            )
+        )
+
+        Switch(
+            checked = false,
+            onCheckedChange = {
+                onTranslateClicked.invoke(it)
+            },
+            thumbContent = {
+                    Icon(
+                        imageVector = Icons.Filled.Check,
+                        contentDescription = null,
+                        modifier = Modifier.size(SwitchDefaults.IconSize),
+                    )
+                },
+            colors = SwitchDefaults.colors(
+                checkedThumbColor = MaterialTheme.colorScheme.primary,
+                checkedTrackColor = MaterialTheme.colorScheme.primaryContainer,
+                uncheckedThumbColor = MaterialTheme.colorScheme.secondary,
+                uncheckedTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+            )
+        )
+
+    }
+
+}
