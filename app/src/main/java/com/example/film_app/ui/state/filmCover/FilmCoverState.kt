0075cf18b8ce9069b92159779eb49228ac1e2681
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-05-20 03:35:53 -1200

    Debug Film Cover State

--- app/src/main/java/com/example/film_app/ui/state/filmCover/FilmCoverState.kt
+++ app/src/main/java/com/example/film_app/ui/state/filmCover/FilmCoverState.kt
@@ -5,7 +5,7 @@ import com.example.film_app.model.database.entities.AllDataEntity
 sealed class FilmCoverState {
 
     data object Idle : FilmCoverState()
-    data class FetchData(val data : List<AllDataEntity>)
-    data class Error(val error : String?)
+    data class FetchData(val data : List<AllDataEntity>) : FilmCoverState()
+    data class Error(val error : String?) : FilmCoverState()
 
 }
